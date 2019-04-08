17e0e5b49dea5c6860809eae2d3b18d32b1984d7
sutakori <yuheng.lu@outlook.com>
2019-04-08 22:15:07 +0800

    first commit

---
+++ lib/absinthe/ecto_enum.ex
@@ -0,0 +1,18 @@
+defmodule Absinthe.EctoEnum do
+  @moduledoc """
+  Documentation for Absinthe.EctoEnum.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Absinthe.EctoEnum.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/absinthe/ecto_enum_test.exs
@@ -0,0 +1,8 @@
+defmodule Absinthe.EctoEnumTest do
+  use ExUnit.Case
+  doctest Absinthe.EctoEnum
+
+  test "greets the world" do
+    assert Absinthe.EctoEnum.hello() == :world
+  end
+end
